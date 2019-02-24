a230db17de8a7f881e3efcef037cd19e21e29250
zhaoyizheng <1411873500@qq.com>
2019-02-24 20:57:14 +0800

    zyz

--- twoGroupsNb/src/Main.java
+++ twoGroupsNb/src/Main.java
@@ -3,4 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    private void test(){
+        System.out.println("hhh");
+    }
 }
