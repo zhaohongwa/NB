756395891f02129a0eea8ab34ccea57fbe4e2eb4
chenxufei <905078298@qq.com>
2019-02-24 21:20:08 +0800

    123

--- twoGroupsNb/src/Main.java
+++ twoGroupsNb/src/Main.java
@@ -3,16 +3,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-
     private void test(){
         System.out.println("hhh");
     }
-
     private void test2(){
         System.out.println("xxx");
     }
-
-
     public void test4(String[] args) {
         System.out.println("XXXOOO");
     }
